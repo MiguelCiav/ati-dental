f73bb0503d3ea359f870b9748e7e5785355f00f1
david <david.delgadop88@gmail.com>
2026-02-23 19:19:05 -0400

    organizar componentes compartidos en ui

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,2 +1,2 @@
-export { default as Sidebar } from './Sidebar';
-export { default as Navbar } from './Navbar';
+export { default as Sidebar } from './ui/Sidebar';
+export { default as Navbar } from './ui/Navbar';
