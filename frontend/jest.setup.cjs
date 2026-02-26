19a38554b825dca88ae728f3ddcb20b9b838e8cd
Miguel Ciavato <miguelciavato@gmail.com>
2026-02-25 20:02:50 -0400

    bugfixes in jest.setup

--- frontend/jest.setup.cjs
+++ frontend/jest.setup.cjs
@@ -2,8 +2,16 @@ const { TextEncoder, TextDecoder } = require('util');
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
-global.Request = Request;
-global.Response = Response;
+// Polyfills mínimos para fetch, Request y Response si no están definidos por jsdom
+if (typeof global.Request === 'undefined') {
+    global.Request = class Request { };
+}
+if (typeof global.Response === 'undefined') {
+    global.Response = class Response { };
+}
+if (typeof global.fetch === 'undefined') {
+    global.fetch = jest.fn(() => Promise.resolve());
+}
 
 // Importar jest-dom globalmente para no tener que hacerlo en cada archivo de test
 require('@testing-library/jest-dom');
